b97e97f733709e60f886fdf1e00ba3dce50ed1f9
aziz mobarak <mobarakaziz9@gmail.com>
2020-10-19 16:54:58 +0100

    add

--- routes.js
+++ routes.js
@@ -15,24 +15,7 @@ router.route('/jobs/:page').get(jobs);
 router.route('/gettoken').post(userInfo, createtoken);
 router.route('/getjobs/:skip/:limit').get(alljobs)
 router.route('/test').post((req, res) => {
-    var jobs = new Jobs({
-        details: {
-            url: "url",
-            title: "title",
-            location: "location",
-            description: 'description',
-        },
-        main: {
-            content: "content",
-            application: "application"
-        }
-    });
-    jobs.save((err, doc) => {
-        if (err) console.log(err);
-        else {
-            res.send(doc)
-        }
-    })
+    res.send('test');
 });
 
 
