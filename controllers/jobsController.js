43214e434b6726e06d7e992c98b58dff0b05afa0
aziz mobarak <mobarakaziz9@gmail.com>
2020-10-28 18:33:08 +0100

    teest

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -9,6 +9,7 @@ const jobs = () => {
         try {
             (async() => {
                 const browser = await puppeteer.launch({
+                    executablePath: "/usr/bin/chromium-browser",
                     timeout: 50000,
                     headless: true,
                     args: ['--no-sandbox']
